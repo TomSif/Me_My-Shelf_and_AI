a3cabdf02e6d2dd21f23a8b6c1e5e45e7a2edfcf
TomSif <thomas.sifferle.programmer@gmail.com>
2026-05-03 08:38:55 +0200

    feat: afficher la liste des parfums avec état vide

---
+++ src/components/fragrance/FragranceList.tsx
@@ -0,0 +1,26 @@
+import type { Fragrance } from "../../types/fragrance";
+import { FragranceCard } from "./FragranceCard";
+
+interface Props {
+  fragrances: Fragrance[];
+}
+
+export function FragranceList({ fragrances }: Props) {
+  if (fragrances.length === 0) {
+    return (
+      <p className="text-sm text-stone-400">
+        Aucun parfum dans ta collection.
+      </p>
+    );
+  }
+
+  return (
+    <ul className="flex flex-col gap-3">
+      {fragrances.map((fragrance) => (
+        <li key={fragrance.id}>
+          <FragranceCard fragrance={fragrance} />
+        </li>
+      ))}
+    </ul>
+  );
+}
